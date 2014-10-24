c1a56fe717230edc9e35f16034b3c0745a1a442a
jlett <jack.maillett@gmail.com>
2014-10-24 09:05:27 -0400

    added test doc image and segmantation file

---
+++ seg.py
@@ -0,0 +1,8 @@
+###########################################################
+#
+#	Input: full page of doc
+#	Output: array of single letter images
+#	Next In Pileline: character recognition (Alberto)
+#
+###########################################################
+#
