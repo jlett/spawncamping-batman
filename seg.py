4fa8e4d2fcd506c10ed449b87fa39363fa0ddee5
jlett <jack.maillett@gmail.com>
2014-11-07 07:54:17 -0500

    added testdoc

--- seg.py
+++ seg.py
@@ -1,32 +1,41 @@
 ###########################################################
 #
-#	Input: full page of doc
+#	Input: full page of doc assumed with no extra noise or deformations
 #	Output: array of single letter images
+#	Previous in Pipeline: denoise and island removal (Jack)
 #	Next In Pileline: character recognition (Alberto)
 #
 ###########################################################
 import matplotlib.pyplot as plt
+import numpy as np
+import skimage
+from skimage import  data, io, img_as_float
+from skimage.filter import threshold_adaptive
+from skimage.viewer import ImageViewer
 
-from skimage import img_as_float, io
-from skimage.filter import threshold_otsu, threshold_adaptive
+img = img_as_float(io.imread("testdoc.png"))
+
+lx, ly = img.shape
+cropped = img[lx / 4: - lx / 4, ly / 4: - ly / 4]
+
+#histo = np.histogram(img, bins=np.arange(0, 256))
+
+#block_size = 40
+#binary_adaptive = threshold_adaptive(img, block_size, offset=10)
 
 
-image = io.imread("testdoc.png");
 
-block_size = 40
-binary_adaptive = threshold_adaptive(image, block_size, offset=10)
-
-fig, axes = plt.subplots(nrows=2, figsize=(7, 8))
-ax0, ax1 = axes
+#DISPLAY STUFF
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(8, 5))
 plt.gray()
 
-ax0.imshow(image)
-ax0.set_title('Image')
+ax[0].imshow(img)
+ax[0].set_title("original")
 
-ax1.imshow(binary_adaptive)
-ax1.set_title('Adaptive thresholding')
+#ax[0, 1].imshow(histo)
+#ax[0, 1].set_title("histogram")
 
-for ax in axes:
-    ax.axis('off')
+ax[1].imshow(cropped)
+ax[1].set_title("cropped")
 
 plt.show()
