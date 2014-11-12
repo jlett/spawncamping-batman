b3c53d6f2b31bd153e0165e1778720ddcbac80d7
jlett <jack.maillett@gmail.com>
2014-11-12 08:31:31 -0500

    produce low res image for use of approximate segmentation

--- seg.py
+++ seg.py
@@ -9,33 +9,48 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import skimage
-from skimage import  data, io, img_as_float
+from skimage import  data, novice, io, img_as_float
 from skimage.filter import threshold_adaptive
 from skimage.viewer import ImageViewer
+import skimage.transform
 
 img = img_as_float(io.imread("testdoc.png"))
+#img = np.fix(img) #rounds values to nearest int
 
 lx, ly = img.shape
-cropped = img[lx / 4: - lx / 4, ly / 4: - ly / 4]
+xRES = int(lx/20) 
+print(xRES)
+yRES = int(ly/20)
+print(yRES)
 
-#histo = np.histogram(img, bins=np.arange(0, 256))
+lowres = skimage.transform.resize(img, (xRES, yRES))
 
-#block_size = 40
-#binary_adaptive = threshold_adaptive(img, block_size, offset=10)
+blackT = 40
 
+img2 = np.copy(img)
+img2[:lx/2, :] = 0
+
+#for i in xrange(lx):
+#	for j in xrange(ly):
+#		val = np.rint(img[i, j])
+i = 0
+j = 0
+for (i,j), value in np.ndenumerate(img):
+	#print(i, j, img[i,j])
+	pass
 
 
 #DISPLAY STUFF
-fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(8, 5))
+fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(8, 5))
 plt.gray()
 
 ax[0].imshow(img)
 ax[0].set_title("original")
 
-#ax[0, 1].imshow(histo)
-#ax[0, 1].set_title("histogram")
+ax[1].imshow(lowres)
+ax[1].set_title("reduced resolution")
 
-ax[1].imshow(cropped)
-ax[1].set_title("cropped")
+ax[2].imshow(img2)
+ax[2].set_title("testimg")
 
 plt.show()
